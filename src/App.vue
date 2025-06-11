e74dd8f610e6a80c3d5c7ca0677578da19825481
Temurbie <temurbie@gmail.com>
2025-06-11 15:26:37 +0500

    vue project 1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>counterL {{ counter }}</p>
+        <div class="d-grid gap-1">
+            <button v-on:click="inc" class="btn btn-success">inc</button>
+            <button v-on:click="dsc" class="btn btn-danger">dsc</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            counter: 2,
+        }
+    },
+    methods:{
+        inc(){
+            this.counter ++
+        },
+        dsc(){
+            if(this.counter <= 0){
+                return this.counter
+            }else{
+
+                this.counter --
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
